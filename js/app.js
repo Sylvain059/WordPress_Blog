1ac09794d4827193e45f0b81c115b052ad5c97cd
Sylvain059 <sylvain.sculier88@gmail.com>
2017-06-19 08:44:25 +0000

    js/app.js/ Add js for menu tabs

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// Fonction pour le menu Tabs.
+function openDistrib(evt, distribName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(distribName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
